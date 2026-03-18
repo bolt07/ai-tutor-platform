fffdd6fd52f1004e1fb215dacad04ac2e981d277
bold07 <itshiteshkumar3816@gmail.com>
2026-03-18 09:01:59 +0530

    added useAudioRecorder hook to listen and pass data to socket

---
+++ src/app/hooks/useAudioRecoder.tsx
@@ -0,0 +1,47 @@
+import { voiceSocket } from "@/src/services/voiceSocketService";
+import { useVoiceState } from "@/src/store/useVoiceStore";
+import { useCallback, useRef } from "react";
+
+export const useAudioRecorder = () => {
+  // make reference of media webapi object
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const startRecording = useCallback(async () => {
+    try {
+      // mediaDeive interface to check mic permission
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      const mediaRecorder = new MediaRecorder(stream);
+      mediaRecorderRef.current = mediaRecorder;
+      // event of mediarecoder (pass audio into blob)
+      mediaRecorder.ondataavailable = (event) => {
+        if (event.data.size > 0) {
+          voiceSocket.sendAudioChunk(event.data);
+        }
+      };
+
+      // chop audio every 250ms
+      mediaRecorder.start(250);
+      useVoiceState.getState().setVoiceState("listening");
+    } catch (error) {
+      console.error("Error accessing microphone:", error);
+      alert("Allow microphone access.");
+    }
+  }, []);
+
+  const stopRecording = useCallback(() => {
+    if (
+      mediaRecorderRef.current &&
+      mediaRecorderRef.current.state !== "inactive"
+    ) {
+      mediaRecorderRef.current.stop();
+
+      // stop all audio track given by getTrack()
+      // getTrack return array of MediaStreamTrack(it contains info about each track) objects
+      mediaRecorderRef.current.stream.getTracks().forEach((track) => {
+        track.stop();
+      });
+
+      mediaRecorderRef.current = null;
+      useVoiceState.getState().setVoiceState("idle");
+    }
+  }, []);
+};
--- src/services/voiceSocketService.tsx
+++ src/services/voiceSocketService.tsx
@@ -17,3 +17,121 @@
 // else
 // maxium attempts reached -> disconnect
 // kal subha karte hai ab ye
+
+import { use } from "react";
+import { useVoiceState } from "../store/useVoiceStore";
+
+// we have to make class and wrap
+class voiceSocketService {
+  private ws: WebSocket | null = null;
+  private reconnectAttempts = 0;
+  private maxReconnectAttpemts = 3;
+  private apiKey: string;
+  private socketURL: string;
+
+  constructor(apiKey: string) {
+    this.apiKey = apiKey;
+    this.socketURL = "";
+  }
+  // whole connections logic inside this
+  connect() {
+    if (
+      this.ws &&
+      (this.ws.readyState === WebSocket.OPEN ||
+        this.ws.readyState === WebSocket.CONNECTING)
+    ) {
+      return;
+    }
+
+    const {
+      setConnectionStatus,
+      setVoiceState,
+      setPartialTranscript,
+      setFinalTranscript,
+    } = useVoiceState();
+
+    setConnectionStatus(
+      this.reconnectAttempts > 0 ? "reconnecting" : "connected",
+    );
+
+    try {
+      this.ws = new WebSocket(this.socketURL);
+
+      this.ws.onopen = () => {
+        console.log("Websocket Connected");
+        this.reconnectAttempts = 0;
+        setConnectionStatus("connected");
+      };
+
+      this.ws.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+
+        if (data.type === "transcript_partial") {
+          setPartialTranscript(data.text);
+          setFinalTranscript("");
+        } else if (data.type === "transcript_final") {
+          setFinalTranscript(data.text);
+          setPartialTranscript("");
+        } else if (data.type == "ai_speaking_start") {
+          setVoiceState("speaking");
+        } else if (data.type === "ai_speaking_stop") {
+          setVoiceState("idle");
+        }
+      };
+
+      this.ws.onclose = (event) => {
+        console.log("Websocket disconnected", event.reason);
+        setConnectionStatus("disconnected");
+        setVoiceState("idle");
+
+        if (!event.wasClean) {
+          this.handleExponentialBackoff();
+        }
+      };
+
+      this.ws.onerror = (error) => {
+        console.log("Websocket error:", error);
+      };
+    } catch (error) {
+      console.error("Failed to connect to socket:", error);
+      this.handleExponentialBackoff();
+    }
+  }
+
+  private handleExponentialBackoff() {
+    if (this.reconnectAttempts < this.maxReconnectAttpemts) {
+      useVoiceState.getState().setConnectionStatus("reconnecting");
+
+      const delay = Math.pow(2, this.reconnectAttempts) * 1000;
+
+      console.log(
+        `Reconnecting in ${delay}ms...(Attemp ${this.reconnectAttempts + 1})`,
+      );
+
+      setTimeout(() => {
+        this.reconnectAttempts++;
+        this.connect();
+      }, delay);
+    } else {
+      console.error("Maximum attempts reached");
+      useVoiceState.getState().setConnectionStatus("disconnected");
+    }
+  }
+
+  sendAudioChunk(chunk: Blob | ArrayBuffer) {
+    if (this.ws && this.ws.readyState === WebSocket.OPEN) {
+      this.ws.send(chunk);
+    }
+  }
+
+  disconnect() {
+    if (this.ws) {
+      this.ws.close(1000, "User manually disconnted");
+      this.ws = null;
+    }
+  }
+}
+
+export const voiceSocket = new voiceSocketService(
+  process.env.NEXT_PUBLIC_VOICE_API || "",
+);
